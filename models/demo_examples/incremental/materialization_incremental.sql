d7586feb03abbf47512c7a6058479e6e45b54054
Isabela Sobral <isabela.sobral@dbtlabs.com>
2024-01-22 19:52:38 +0000

    fix incremental

--- models/demo_examples/incremental/materialization_incremental.sql
+++ models/demo_examples/incremental/materialization_incremental.sql
@@ -6,8 +6,8 @@
 }}
 
 select order_id as order_id, customer_id as customer_id, status as status, order_date as order_date, modified_at as modified_at
-from {{ ref('raw_orders') }}
+from {{ ref('raw_orders') }} as raw
 
 {% if is_incremental() %}
-    where modified_at > (select max(modified_at) from {{ this }}) 
+    where raw.modified_at > (select max(modified_at) from {{ this }}) 
 {% endif %}
