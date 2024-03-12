f10aac79bc11ef25c892bba8c5a53b945bafc615
Isabela Sobral <isabela.sobral@dbtlabs.com>
2024-03-12 19:46:34 +0000

    added new metric

--- models/marts/intermediate/order_items.sql
+++ models/marts/intermediate/order_items.sql
@@ -12,31 +12,6 @@ line_item as (
 )
 select 
 
-    line_item.order_item_key,
-    orders.order_key,
-    orders.customer_key,
-    line_item.part_key,
-    line_item.supplier_key,
-    orders.order_date,
-    orders.status_code as order_status_code,
-    
-    
-    line_item.return_flag,
-    
-    line_item.line_number,
-    line_item.status_code as order_item_status_code,
-    line_item.ship_date,
-    line_item.commit_date,
-    line_item.receipt_date,
-    line_item.ship_mode,
-    line_item.extended_price,
-    line_item.quantity,
-    
-    -- extended_price is actually the line item total,
-    -- so we back out the extended price per item
-    (line_item.extended_price/nullif(line_item.quantity, 0)){{ money() }} as base_price,
-    line_item.discount_percentage,
-    (base_price * (1 - line_item.discount_percentage)){{ money() }} as discounted_price,
 
     line_item.extended_price as gross_item_sales_amount,
     (line_item.extended_price * (1 - line_item.discount_percentage)){{ money() }} as discounted_item_sales_amount,
