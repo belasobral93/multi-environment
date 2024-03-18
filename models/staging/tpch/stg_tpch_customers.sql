f5ed46aa8c1cb84906f2aa28900a312017718f9b
Isabela Sobral <isabela.sobral@dbtlabs.com>
2024-03-18 23:39:55 +0000

    test

--- models/staging/tpch/stg_tpch_customers.sql
+++ models/staging/tpch/stg_tpch_customers.sql
@@ -4,7 +4,7 @@ with source as (
 
     select * from {{ source('tpch', 'customer') }}
 
-),brand_news as (
+),brand_new as (
 
     select
     
@@ -20,5 +20,5 @@ with source as (
     from source
 
 )
-select * from brand_news
+select * from brand_new
 
