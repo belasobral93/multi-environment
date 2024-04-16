ed0535c747a1454c60bc8ffcb3509771c28310e5
Isabela Sobral <isabela.sobral@dbtlabs.com>
2024-04-16 16:37:12 +0000

    test

--- models/marts/aggregates/monthly_gross_revenue.sql
+++ models/marts/aggregates/monthly_gross_revenue.sql
@@ -1,5 +1,5 @@
 select 
     date_trunc(MONTH, fct_order_items.order_date) as order_month
-    , sum(fct_order_items.item_tax_amount) as gross_revenue
+    , sum(fct_order_items.quantity) as gross_revenue
 from {{ ref('fct_order_items') }}
 group by 1
