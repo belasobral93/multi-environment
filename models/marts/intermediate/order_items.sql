2227c7f3fc1810870e0542359dfabc046ec4cc3b
Isabela Sobral <isabela.sobral@dbtlabs.com>
2024-07-09 03:15:14 +0000

    data diff

--- models/marts/intermediate/order_items.sql
+++ models/marts/intermediate/order_items.sql
@@ -45,8 +45,8 @@ select
     line_item.tax_rate,
     ((gross_item_sales_amount + item_discount_amount) * line_item.tax_rate){{ money() }} as item_tax_amount,
     (
-        gross_item_sales_amount + 
-        item_discount_amount 
+        gross_item_sales_amount 
+        -- + item_discount_amount 
         -- + item_tax_amount
     ){{ money() }} as net_item_sales_amount
 
