2e745d3427b44204a38ead380b928c1bfe722196
Isabela Sobral <isabela.sobral@dbtlabs.com>
2025-02-22 00:47:23 +0000

    test

--- models/marts/aggregates/gross_revenue.sql
+++ models/marts/aggregates/gross_revenue.sql
@@ -6,6 +6,6 @@
 
 select
     order_key,
-    sum(fct_order_items.net_item_sales_amount) as gross_revenues
+    sum(fct_order_items.net_item_sales_amount) * 1.5 as gross_revenues
 from {{ ref('fct_order_items') }}
 group by 1
