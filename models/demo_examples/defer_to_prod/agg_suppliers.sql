ddb12930eb811be4c7b73f44f719aff6adb8f5e7
Isabela Sobral <isabela.sobral@dbtlabs.com>
2025-04-09 15:58:20 +0000

    test advanced CI

--- models/demo_examples/defer_to_prod/agg_suppliers.sql
+++ models/demo_examples/defer_to_prod/agg_suppliers.sql
@@ -13,7 +13,7 @@ with supplier_aggregated as (
         count(distinct supplier_key) as supplier_count,
         sum(account_balance) as total_account_balance
     from
-        {{ ref('dim_suppliers') }}  -- when using defer, will reference the prod version of upstream model
+        {{ ref('dim_suppliers') }}  -- when using defer, will reference the higher env version of upstream model
     group by
         region,
         nation
