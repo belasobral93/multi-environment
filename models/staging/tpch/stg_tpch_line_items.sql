f356b1648456ca5ad5fe182035f3102df041494c
Isabela Sobral <isabela.sobral@dbtlabs.com>
2025-05-16 21:16:45 +0000

    advanced ci

--- models/staging/tpch/stg_tpch_line_items.sql
+++ models/staging/tpch/stg_tpch_line_items.sql
@@ -9,23 +9,23 @@ renamed as (
     select
     
         {{ dbt_utils.generate_surrogate_key(
-        --     ['l_orderkey', 
-        --     'l_linenumber']) }}
-        --         as order_item_key,
-        -- l_orderkey as order_key,
-        -- l_partkey as part_key,
-        -- l_suppkey as supplier_key,
-        -- l_linenumber as line_number,
-        -- l_quantity as quantity,
-        -- l_extendedprice as extended_price,
-        -- l_discount as discount_percentage,
-        -- l_tax as tax_rate,
+            ['l_orderkey', 
+            'l_linenumber']) }}
+                as order_item_key,
+        l_orderkey as order_key,
+        l_partkey as part_key,
+        l_suppkey as supplier_key,
+        l_linenumber as line_number,
+        l_quantity as quantity,
+        l_extendedprice as extended_price,
+        l_discount as discount_percentage,
+        l_tax as tax_rate,
         
         case 
             when l_returnflag in ('R') then 'returned'
-        --     when l_returnflag in ('A') then 'accepted'
-        --     else 'unknown'
-        -- end as return_flag, 
+            when l_returnflag in ('A') then 'accepted'
+            else 'unknown'
+        end as return_flag, 
 
         case 
             when return_flag = 'accepted' then false
