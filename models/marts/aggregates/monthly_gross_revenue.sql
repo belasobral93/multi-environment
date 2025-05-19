e0c862c00fdd0e180646a75a9ed343d33a946b42
Isabela Sobral <isabela.sobral@dbtlabs.com>
2025-05-19 20:24:51 +0000

    new gross revenue definition

--- models/marts/aggregates/monthly_gross_revenue.sql
+++ models/marts/aggregates/monthly_gross_revenue.sql
@@ -1,5 +1,8 @@
 select
     date_trunc(month, fct_order_items.order_date) as order_month,
-    sum(fct_order_items.net_item_sales_amount * 1.5) as gross_revenue
+    sum(fct_order_items.net_item_sales_amount * 1.5) as gross_revenue,
+    sum(fct_order_items.net_item_sales_amount * 1.5) 
+        - lag(sum(fct_order_items.net_item_sales_amount * 1.5)) 
+        over (order by date_trunc(month, fct_order_items.order_date)) as revenue_growth
 from {{ ref('fct_order_items') }}
 group by 1
