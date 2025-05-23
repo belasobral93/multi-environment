87e2f6f2411463cb0e3ee1afb0c5db7a600031f5
Isabela Sobral <isabela.sobral@dbtlabs.com>
2025-05-23 18:37:00 +0000

    adjusted gross revenue

--- models/marts/aggregates/monthly_gross_revenue.sql
+++ models/marts/aggregates/monthly_gross_revenue.sql
@@ -1,8 +1,5 @@
-select
-    date_trunc(month, fct_order_items.order_date) as order_month,
-    sum(fct_order_items.net_item_sales_amount * 1.5) as gross_revenue,
-    sum(fct_order_items.net_item_sales_amount * 1.5) 
-        - lag(sum(fct_order_items.net_item_sales_amount * 1.5)) 
-        over (order by date_trunc(month, fct_order_items.order_date)) as revenue_growth
+select 
+    date_trunc(MONTH, fct_order_items.order_date) as order_month
+    -- , sum(fct_order_items.NET_ITEM_SALES_AMOUNT) as gross_revenue
 from {{ ref('fct_order_items') }}
 group by 1
